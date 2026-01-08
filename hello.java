8d157d757ca5802932dc4d68628e24f3ff9ec370
sowmya-rebbavarapu <sowmyarebbavarapu@gmail.com>
2026-01-08 14:58:46 +0530

    sum

--- hello.java
+++ hello.java
@@ -1,5 +1,8 @@
 public class hello{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner sc=new Scanner();
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println(a+b);
     }
 }
