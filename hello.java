a4e83e5e78add1413ff1d5cb132b2b4e580ec459
sowmya-rebbavarapu <sowmyarebbavarapu@gmail.com>
2026-01-08 15:02:18 +0530

    addition
    Updated the Addition class to prompt for user input and display the sum.

--- hello.java
+++ hello.java
@@ -1,9 +1,17 @@
-import java.util.*;
-public class hello{
+import java.util.Scanner;
+
+public class Addition {
     public static void main(String[] args) {
-        Scanner sc=new Scanner();
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        System.out.println(a+b);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
     }
 }
